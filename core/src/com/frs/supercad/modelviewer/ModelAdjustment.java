1801dfd14bab7e9f6855eea42a4e33c51a0eea76
nadhem ba <nadhembenali@outlook.fr>
2020-06-09 17:16:11 +0100

    adjusting model translation and scale

---
+++ core/src/com/frs/supercad/modelviewer/ModelAdjustment.java
@@ -0,0 +1,11 @@
+package com.frs.supercad.modelviewer;
+
+import com.badlogic.gdx.math.Quaternion;
+import com.badlogic.gdx.math.Vector3;
+
+public class ModelAdjustment {
+
+	Vector3 scale;
+	Vector3 translation;
+	Quaternion rotation;
+}
--- desktop/src/com/frs/supercad/desktop/MainFrame.java
+++ desktop/src/com/frs/supercad/desktop/MainFrame.java
@@ -9,6 +9,7 @@ import com.badlogic.gdx.Game;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.badlogic.gdx.backends.lwjgl.LwjglCanvas;
 import com.badlogic.gdx.graphics.g3d.Model;
+import com.badlogic.gdx.math.Vector3;
 import com.frs.supercad.ModelConverter;
 import com.frs.supercad.assets.ModelAsset;
 import com.frs.supercad.modelviewer.ModelInfo;
@@ -17,9 +18,10 @@ import com.frs.supercad.utilities.IOUtilities;
 import java.awt.*;
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
-import java.beans.PropertyChangeSupport;
 import java.io.*;
 import javax.swing.*;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
 import javax.swing.filechooser.FileSystemView;
 
 /**
@@ -30,6 +32,10 @@ public class MainFrame extends JFrame {
 	Game app = new ModelConverter();
 	LwjglCanvas canvas;
 	public ModelInfo info = ModelConverter.modelInfo;
+
+
+
+
 
 
 	public MainFrame(LwjglApplicationConfiguration config) {
@@ -59,6 +65,20 @@ public class MainFrame extends JFrame {
 				});
 			}
 		});
+
+		addTextFieldListener();
+	}
+
+	private void addTextFieldListener() {
+		this.tfTranslZ.getDocument().addDocumentListener(new CustomDocumentListener());
+		this.tfTranslY.getDocument().addDocumentListener(new CustomDocumentListener());
+		this.tfTranslX.getDocument().addDocumentListener(new CustomDocumentListener());
+
+		this.tfScaleX.getDocument().addDocumentListener(new CustomDocumentListener());
+		this.tfScaleY.getDocument().addDocumentListener(new CustomDocumentListener());
+		this.tfScaleZ.getDocument().addDocumentListener(new CustomDocumentListener());
+
+
 	}
 
 	private void addCanvas() {
@@ -104,6 +124,32 @@ public class MainFrame extends JFrame {
 
 	}
 
+	public void updateModelInstance(){
+	Vector3 scale = new Vector3();
+		Vector3 translation = new Vector3();
+		try{
+			scale.x = Float.parseFloat(this.tfScaleX.getText());
+			scale.y = Float.parseFloat(this.tfScaleY.getText());
+			scale.z = Float.parseFloat(this.tfScaleZ.getText());
+
+			translation.x = Float.parseFloat(this.tfTranslX.getText());
+			translation.y = Float.parseFloat(this.tfTranslY.getText());
+			translation.z = Float.parseFloat(this.tfTranslZ.getText());
+
+		}catch(Exception e){
+			scale = new Vector3();
+			translation = new Vector3();
+		}
+
+
+		ViewerController.instance.transform.setToScaling(scale);
+		ViewerController.instance.calculateTransforms();
+		ViewerController.instance.transform.translate(translation);
+	}
+
+
+
+
 	private void initComponents() {
 		// JFormDesigner - Component initialization - DO NOT MODIFY  //GEN-BEGIN:initComponents
 		// Generated using JFormDesigner Evaluation license - sana
@@ -113,10 +159,9 @@ public class MainFrame extends JFrame {
 		southernPanel = new JPanel();
 		canvasP = new JPanel();
 		controlPanel = new JPanel();
-		panel1 = new JPanel();
 		tabbedPane1 = new JTabbedPane();
-		panel2 = new JPanel();
-		label1 = new JLabel();
+		panel1 = new JPanel();
+		label9 = new JLabel();
 		panel3 = new JPanel();
 		label2 = new JLabel();
 		tfScaleX = new JTextField();
@@ -132,7 +177,6 @@ public class MainFrame extends JFrame {
 		tfTranslY = new JTextField();
 		label8 = new JLabel();
 		tfTranslZ = new JTextField();
-		label9 = new JLabel();
 
 		//======== this ========
 		setMinimumSize(new Dimension(720, 720));
@@ -160,12 +204,11 @@ public class MainFrame extends JFrame {
 			southernPanel.setBackground(new Color(51, 51, 51));
 			southernPanel.setMinimumSize(new Dimension(720, 30));
 			southernPanel.setPreferredSize(new Dimension(720, 50));
-			southernPanel.setBorder ( new javax . swing. border .CompoundBorder ( new javax . swing. border .TitledBorder ( new javax . swing.
-			border .EmptyBorder ( 0, 0 ,0 , 0) ,  "JFor\u006dDesi\u0067ner \u0045valu\u0061tion" , javax. swing .border . TitledBorder. CENTER
-			,javax . swing. border .TitledBorder . BOTTOM, new java. awt .Font ( "Dia\u006cog", java .awt . Font
-			. BOLD ,12 ) ,java . awt. Color .red ) ,southernPanel. getBorder () ) ); southernPanel. addPropertyChangeListener(
-			new java. beans .PropertyChangeListener ( ){ @Override public void propertyChange (java . beans. PropertyChangeEvent e) { if( "bord\u0065r"
-			.equals ( e. getPropertyName () ) )throw new RuntimeException( ) ;} } );
+			southernPanel.setBorder(new javax.swing.border.CompoundBorder(new javax.swing.border.TitledBorder(new javax.swing.border.EmptyBorder(
+			0,0,0,0), "JFor\u006dDesi\u0067ner \u0045valu\u0061tion",javax.swing.border.TitledBorder.CENTER,javax.swing.border.TitledBorder
+			.BOTTOM,new java.awt.Font("Dia\u006cog",java.awt.Font.BOLD,12),java.awt.Color.
+			red),southernPanel. getBorder()));southernPanel. addPropertyChangeListener(new java.beans.PropertyChangeListener(){@Override public void propertyChange(java.
+			beans.PropertyChangeEvent e){if("bord\u0065r".equals(e.getPropertyName()))throw new RuntimeException();}});
 			southernPanel.setLayout(null);
 
 			{
@@ -197,116 +240,107 @@ public class MainFrame extends JFrame {
 			controlPanel.setBackground(SystemColor.windowText);
 			controlPanel.setLayout(new BorderLayout());
 
-			//======== panel1 ========
+			//======== tabbedPane1 ========
 			{
-				panel1.setBackground(SystemColor.windowText);
-				panel1.setLayout(new GridLayout(6, 2, 2, 2));
 
-				//======== tabbedPane1 ========
+				//======== panel1 ========
 				{
+					panel1.setBackground(SystemColor.windowText);
+					panel1.setLayout(new GridLayout(6, 2, 2, 2));
 
-					//======== panel2 ========
+					//---- label9 ----
+					label9.setText("scale");
+					label9.setForeground(SystemColor.text);
+					panel1.add(label9);
+
+					//======== panel3 ========
 					{
-						panel2.setLayout(new GridLayout(0, 2, 10, 2));
+						panel3.setBackground(SystemColor.desktop);
+						panel3.setLayout(new GridLayout(0, 6, 5, 0));
 
-						//---- label1 ----
-						label1.setText("scale");
-						panel2.add(label1);
+						//---- label2 ----
+						label2.setText("X:");
+						label2.setHorizontalAlignment(SwingConstants.CENTER);
+						label2.setForeground(SystemColor.text);
+						panel3.add(label2);
+
+						//---- tfScaleX ----
+						tfScaleX.setForeground(Color.white);
+						tfScaleX.setBackground(SystemColor.desktop);
+						panel3.add(tfScaleX);
+
+						//---- label3 ----
+						label3.setText("Y:");
+						label3.setHorizontalAlignment(SwingConstants.CENTER);
+						label3.setForeground(Color.white);
+						panel3.add(label3);
+
+						//---- tfScaleY ----
+						tfScaleY.setBackground(SystemColor.desktop);
+						tfScaleY.setForeground(Color.white);
+						panel3.add(tfScaleY);
+
+						//---- label4 ----
+						label4.setText("Z:");
+						label4.setHorizontalAlignment(SwingConstants.CENTER);
+						label4.setForeground(Color.white);
+						panel3.add(label4);
+
+						//---- tfScaleZ ----
+						tfScaleZ.setBackground(Color.black);
+						tfScaleZ.setForeground(Color.white);
+						panel3.add(tfScaleZ);
 					}
-					tabbedPane1.addTab("text", panel2);
+					panel1.add(panel3);
+
+					//---- label5 ----
+					label5.setText("translation");
+					label5.setForeground(SystemColor.text);
+					panel1.add(label5);
+
+					//======== panel4 ========
+					{
+						panel4.setBackground(SystemColor.desktop);
+						panel4.setLayout(new GridLayout(0, 6, 5, 0));
+
+						//---- label6 ----
+						label6.setText("X:");
+						label6.setHorizontalAlignment(SwingConstants.CENTER);
+						label6.setForeground(SystemColor.text);
+						panel4.add(label6);
+
+						//---- tfTranslX ----
+						tfTranslX.setForeground(Color.white);
+						tfTranslX.setBackground(SystemColor.desktop);
+						panel4.add(tfTranslX);
+
+						//---- label7 ----
+						label7.setText("Y:");
+						label7.setHorizontalAlignment(SwingConstants.CENTER);
+						label7.setForeground(Color.white);
+						panel4.add(label7);
+
+						//---- tfTranslY ----
+						tfTranslY.setBackground(SystemColor.desktop);
+						tfTranslY.setForeground(Color.white);
+						panel4.add(tfTranslY);
+
+						//---- label8 ----
+						label8.setText("Z:");
+						label8.setHorizontalAlignment(SwingConstants.CENTER);
+						label8.setForeground(Color.white);
+						panel4.add(label8);
+
+						//---- tfTranslZ ----
+						tfTranslZ.setBackground(Color.black);
+						tfTranslZ.setForeground(Color.white);
+						panel4.add(tfTranslZ);
+					}
+					panel1.add(panel4);
 				}
-				panel1.add(tabbedPane1);
-
-				//======== panel3 ========
-				{
-					panel3.setBackground(SystemColor.desktop);
-					panel3.setLayout(new GridLayout(0, 6, 5, 0));
-
-					//---- label2 ----
-					label2.setText("X:");
-					label2.setHorizontalAlignment(SwingConstants.CENTER);
-					label2.setForeground(SystemColor.text);
-					panel3.add(label2);
-
-					//---- tfScaleX ----
-					tfScaleX.setForeground(Color.white);
-					tfScaleX.setBackground(SystemColor.desktop);
-					panel3.add(tfScaleX);
-
-					//---- label3 ----
-					label3.setText("Y:");
-					label3.setHorizontalAlignment(SwingConstants.CENTER);
-					label3.setForeground(Color.white);
-					panel3.add(label3);
-
-					//---- tfScaleY ----
-					tfScaleY.setBackground(SystemColor.desktop);
-					tfScaleY.setForeground(Color.white);
-					panel3.add(tfScaleY);
-
-					//---- label4 ----
-					label4.setText("Z:");
-					label4.setHorizontalAlignment(SwingConstants.CENTER);
-					label4.setForeground(Color.white);
-					panel3.add(label4);
-
-					//---- tfScaleZ ----
-					tfScaleZ.setBackground(Color.black);
-					tfScaleZ.setForeground(Color.white);
-					panel3.add(tfScaleZ);
-				}
-				panel1.add(panel3);
-
-				//---- label5 ----
-				label5.setText("translation");
-				label5.setForeground(SystemColor.text);
-				panel1.add(label5);
-
-				//======== panel4 ========
-				{
-					panel4.setBackground(SystemColor.desktop);
-					panel4.setLayout(new GridLayout(0, 6, 5, 0));
-
-					//---- label6 ----
-					label6.setText("X:");
-					label6.setHorizontalAlignment(SwingConstants.CENTER);
-					label6.setForeground(SystemColor.text);
-					panel4.add(label6);
-
-					//---- tfTranslX ----
-					tfTranslX.setForeground(Color.white);
-					tfTranslX.setBackground(SystemColor.desktop);
-					panel4.add(tfTranslX);
-
-					//---- label7 ----
-					label7.setText("Y:");
-					label7.setHorizontalAlignment(SwingConstants.CENTER);
-					label7.setForeground(Color.white);
-					panel4.add(label7);
-
-					//---- tfTranslY ----
-					tfTranslY.setBackground(SystemColor.desktop);
-					tfTranslY.setForeground(Color.white);
-					panel4.add(tfTranslY);
-
-					//---- label8 ----
-					label8.setText("Z:");
-					label8.setHorizontalAlignment(SwingConstants.CENTER);
-					label8.setForeground(Color.white);
-					panel4.add(label8);
-
-					//---- tfTranslZ ----
-					tfTranslZ.setBackground(Color.black);
-					tfTranslZ.setForeground(Color.white);
-					panel4.add(tfTranslZ);
-				}
-				panel1.add(panel4);
-
-				//---- label9 ----
-				label9.setText("scale");
-				panel1.add(label9);
+				tabbedPane1.addTab("text", panel1);
 			}
-			controlPanel.add(panel1, BorderLayout.CENTER);
+			controlPanel.add(tabbedPane1, BorderLayout.NORTH);
 		}
 		contentPane.add(controlPanel, BorderLayout.EAST);
 		pack();
@@ -322,10 +356,9 @@ public class MainFrame extends JFrame {
 	private JPanel southernPanel;
 	private JPanel canvasP;
 	private JPanel controlPanel;
-	private JPanel panel1;
 	private JTabbedPane tabbedPane1;
-	private JPanel panel2;
-	private JLabel label1;
+	private JPanel panel1;
+	private JLabel label9;
 	private JPanel panel3;
 	private JLabel label2;
 	private JTextField tfScaleX;
@@ -341,6 +374,47 @@ public class MainFrame extends JFrame {
 	private JTextField tfTranslY;
 	private JLabel label8;
 	private JTextField tfTranslZ;
-	private JLabel label9;
 	// JFormDesigner - End of variables declaration  //GEN-END:variables
+
+	class CustomDocumentListener implements DocumentListener{
+		@Override
+		public void insertUpdate(DocumentEvent e) {
+			System.out.println("handling insert  ...");
+			SwingUtilities.invokeLater(new Runnable() {
+				@Override
+				public void run() {
+					if(ViewerController.instance != null)
+						updateModelInstance();
+				}
+			});
+		}
+
+		@Override
+		public void removeUpdate(DocumentEvent e) {
+			System.out.println("handling remove  ...");
+			SwingUtilities.invokeLater(new Runnable() {
+				@Override
+				public void run() {
+					if(ViewerController.instance != null)
+						updateModelInstance();
+				}
+			});
+
+
+		}
+
+		@Override
+		public void changedUpdate(DocumentEvent e) {
+			System.out.println("handling change  ...");
+			SwingUtilities.invokeLater(new Runnable() {
+				@Override
+				public void run() {
+					if(ViewerController.instance != null)
+						updateModelInstance();
+				}
+			});
+		}
+	}
+
 }
+
